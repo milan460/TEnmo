33f328ed23fae665172f10705a2ffea66ee2e6e5
Adam Walker <abonnett1@gmail.com>
2023-07-06 17:04:01 -0400

    Passing over driving

--- tenmo-server/src/main/java/com/techelevator/tenmo/controller/TransferDtoController.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/controller/TransferDtoController.java
@@ -2,15 +2,61 @@ package com.techelevator.tenmo.controller;
 
 import com.techelevator.tenmo.dao.AccountDao;
 import com.techelevator.tenmo.dao.TransferDtoDao;
+import com.techelevator.tenmo.dao.UserDao;
+import com.techelevator.tenmo.model.TransferDto;
+import com.techelevator.tenmo.model.User;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+import static java.math.BigDecimal.valueOf;
 
 @RestController
 public class TransferDtoController {
     private TransferDtoDao transferDtoDao;
+    private final UserDao userDao;
+    private final AccountDao accountDao;
 
-    public TransferDtoController(TransferDtoDao transferDtoDao){
+    public TransferDtoController(TransferDtoDao transferDtoDao, UserDao userDao, AccountDao accountDao){
         this.transferDtoDao = transferDtoDao;
+        this.userDao = userDao;
+        this.accountDao = accountDao;
     }
 
+    @RequestMapping (path = "/Send", method = RequestMethod.POST)
+    public void makeTransfer(@RequestBody int accountToId, @RequestBody BigDecimal amount){
+        Authentication authentication=SecurityContextHolder.getContext().getAuthentication();
 
+        User user = userDao.getUserByUsername(authentication.getName());
+
+        if(user.getId()==accountToId){
+            throw new IllegalArgumentException("Cannot send money to yourself");
+        }
+        BigDecimal zero = BigDecimal.valueOf(0);
+        if (!(amount.compareTo(zero) ==1)) {
+            throw new IllegalArgumentException("Amount sent must be a non zero positive value");
+        }
+        BigDecimal balance = accountDao.getAccountById(user.getId()).getBalance();
+        if ((amount.compareTo(balance)==1)){
+            throw new IllegalArgumentException("Cannot transfer more money than your current balance");
+        }
+
+        TransferDto transferDto = new TransferDto();
+        transferDto.setAccountFrom(user.getId()); // authentication to pull the account ID
+        transferDto.setTransferTypeId(2);
+        transferDto.setTransferStatusId(1);
+        transferDto.setAccountTo(accountToId);
+        transferDto.setAmount(amount);
+
+        transferDtoDao.sendTransfer(transferDto);
+    }
+
+    // path /transfers
+
+    // path /transfers/{id}
 }
--- tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcTransferDtoDao.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcTransferDtoDao.java
@@ -1,21 +1,56 @@
 package com.techelevator.tenmo.dao;
 
+import com.techelevator.tenmo.exception.DaoException;
 import com.techelevator.tenmo.model.TransferDto;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.jdbc.CannotGetJdbcConnectionException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
 
-import java.math.BigDecimal;
-
+@Component
 public class JdbcTransferDtoDao implements TransferDtoDao {
+    private final JdbcTemplate jdbcTemplate;
+
+    public JdbcTransferDtoDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
     @Override
-    public TransferDto sendTransfer(int accountToId, BigDecimal amount) {
-        TransferDto transferDto = new TransferDto();
-        transferDto.setAccountFrom(01);
-        transferDto.setTransferTypeId(2);
-        transferDto.setTransferStatusId(1);
-        transferDto.setAccountTo(accountToId);
-        transferDto.setAmount(amount);
+    public TransferDto sendTransfer(TransferDto transferDto) {
+        TransferDto transfer = transferDto;
 
+        // post the transfer to the transfer table
+        String transferSql = "INSERT INTO transfer (transfer_type_id, transfer_status_id, account_from, account_to, amount) VALUES (?, ?, ?, ?, ?) RETURNING transfer_id";
+        try {
+             transfer.setId( jdbcTemplate.queryForObject(transferSql, int.class,transferDto.getTransferTypeId(),transferDto.getTransferStatusId(),transferDto.getAccountFrom(),transferDto.getAccountTo(),transferDto.getAmount()));
+        } catch (CannotGetJdbcConnectionException e){
+            throw new DaoException("Unable to connect to Server Database",e);
+        } catch (DataIntegrityViolationException e){
+            throw new DaoException("Data Integrity Violation",e);
+        }
 
-        return null;
+        // decrease the sender's deniro
+        String decreaseSql = "UPDATE account SET balance = balance - ? WHERE account_id = ?";
+        try {
+            jdbcTemplate.update(decreaseSql, transferDto.getAmount(),transferDto.getAccountFrom());
+        } catch (CannotGetJdbcConnectionException e){
+            throw new DaoException("Unable to connect to Server Database",e);
+        } catch (DataIntegrityViolationException e){
+            throw new DaoException("Data Integrity Violation",e);
+        }
+
+        // increase the recipients Mulah
+        String increaseSql = "UPDATE account SET balance = balance + ? WHERE account_id = ?";
+        try {
+            jdbcTemplate.update(decreaseSql, transferDto.getAmount(),transferDto.getAccountTo());
+        } catch (CannotGetJdbcConnectionException e){
+            throw new DaoException("Unable to connect to Server Database",e);
+        } catch (DataIntegrityViolationException e){
+            throw new DaoException("Data Integrity Violation",e);
+        }
+
+        return transfer;
     }
+
+
 }
