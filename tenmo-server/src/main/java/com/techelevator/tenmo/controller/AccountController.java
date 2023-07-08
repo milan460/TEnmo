63957289151b8c4a81daf179a63e12a57231b7c3
Adam Walker <abonnett1@gmail.com>
2023-07-08 18:06:09 -0400

    updated account controller on server

--- tenmo-server/src/main/java/com/techelevator/tenmo/controller/AccountController.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/controller/AccountController.java
@@ -1,20 +1,25 @@
 package com.techelevator.tenmo.controller;
 
 import com.techelevator.tenmo.dao.AccountDao;
+import com.techelevator.tenmo.dao.UserDao;
 import com.techelevator.tenmo.model.Account;
+import com.techelevator.tenmo.model.User;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.Map;
 
 @RestController
 @PreAuthorize("isAuthenticated()")
 public class AccountController {
     private AccountDao accountDao;
+    private final UserDao userDao;
 
-    public AccountController(AccountDao accountDao){
+    public AccountController(AccountDao accountDao, UserDao userDao){
         this.accountDao = accountDao;
+        this.userDao = userDao;
     }
 
     //create a path for id
@@ -32,5 +37,12 @@ public class AccountController {
     @RequestMapping(path = "/account", method = RequestMethod.GET)
     public Map<Integer, String> listAccountByUserName(@RequestParam (required = false) String username){
         return accountDao.getListOfUserAndId(username);
+    }
+
+    @RequestMapping(path = "/account/me", method = RequestMethod.GET)
+    public Account myAccount(){
+        Authentication authentication= SecurityContextHolder.getContext().getAuthentication();
+        User user = userDao.getUserByUsername(authentication.getName());
+        return accountDao.getAccountByUserId(user.getId());
     }
 }
